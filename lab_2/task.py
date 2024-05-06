bb532e1c3b1a244456d8e60ae498688e4cdf64e8
Анастасия Мытарева <nastya.mytareva.04@gmail.com>
2024-05-06 19:19:34 +0400

    test for the same consecutive bits

--- lab_2/task.py
+++ lab_2/task.py
@@ -23,11 +23,28 @@ def frequency_bitwise_test(path: str) -> float:
             summa += -1
     summa = abs(summa)/sqrt(len(sequence))
     return erfc(summa/sqrt(2))
-    
+
+
+def test_for_same_consecutive_bits(path: str) -> float:
+    sequence: str = read_file(path)
+    percentage_of_units: float = sequence.count("1")/len(sequence)
+    if not abs(percentage_of_units - 0.5) < (2 / sqrt(len(sequence))):
+        return 0
+    else:
+        v_n: int = 0
+        for i in range(len(sequence) - 1):
+            if not (sequence[i] == sequence[i + 1]):
+                v_n += 1
+        p_value: float = erfc(abs(v_n - 2 * len(sequence) * percentage_of_units * (1 - percentage_of_units)) / 
+                            (2 * sqrt(2 * len(sequence)) * percentage_of_units * (1 - percentage_of_units)))
+        return p_value
+
 
 def main() -> None:
-    res: float = frequency_bitwise_test("C:/Users/nasty/isb/lab_2/sequence_c.txt")
-    print(res)
+    res1: float = frequency_bitwise_test("C:/Users/nasty/isb/lab_2/sequence_c.txt")
+    res2: float = test_for_same_consecutive_bits("C:/Users/nasty/isb/lab_2/sequence_c.txt")
+    print(res1)
+    print(res2)
 
 
 if __name__ == "__main__":
